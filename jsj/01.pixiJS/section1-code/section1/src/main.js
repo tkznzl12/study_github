5832ef5535e7e57e6a2d4bdedc3beeec8c473379
tkznzl12 <tkznzl12@naver.com>
2026-03-10 13:03:40 +0900

    pixi.js강의 기본구조 수강 완료

--- jsj/01.pixiJS/section1-code/section1/src/main.js
+++ jsj/01.pixiJS/section1-code/section1/src/main.js
@@ -1,10 +1,18 @@
+import './style.css';
 import * as PIXI from 'pixi.js';
 
+
+//pixijs의 가장 기본적인 구조
+(async()=> {
 //Application
-const app = new PIXI.Application();
+    const app = new PIXI.Application();
 
-app.init({
-    background:'royalblue'
-});
+    await app.init({
+        background:'royalblue'
+    });
 
-document.body.appendChild(app.canvas);
+    app.canvas.id = 'app-canvas';
+
+    document.body.appendChild(app.canvas);
+}
+)()
